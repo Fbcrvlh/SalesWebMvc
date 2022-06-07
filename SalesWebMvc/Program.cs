920324700a14b5276a96f690d7068377d5a5accd
Fabio Carvalho <fabio.carvalho@askblue.pt>
2022-06-07 14:49:07 +0100

    Create Department model and controller

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMvcContext"))));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
