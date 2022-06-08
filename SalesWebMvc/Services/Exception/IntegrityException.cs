c2a804d74c5688a30ad9a9f56a3f5bb602559dc9
Fabio Carvalho <fabio.carvalho@askblue.pt>
2022-06-08 15:35:02 +0100

    IntegratyException

---
+++ SalesWebMvc/Services/Exception/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿namespace SalesWebMvc.Services.Exception
+{
+    public class IntegrityException: ApplicationException
+
+    {
+
+        public IntegrityException (string message): base(message)
+        {
+
+        }
+    }
+}
